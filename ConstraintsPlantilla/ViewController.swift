7e9d55c25b8ec62d866b3f7c632a420d19b31e52
Alumno <ulsa@Pol.local>
2018-09-06 13:15:33 -0700

    Recibidores

--- ConstraintsPlantilla/ViewController.swift
+++ ConstraintsPlantilla/ViewController.swift
@@ -7,9 +7,21 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player = AVAudioPlayer()
+    var imagen = UIImage()
+    var audio : String = ""
+    
+    @IBAction func doTapImg1(_ sender: Any) {
+        audio = "vaca"
+        imagen = ""
+        
+        Tap(imagen: imagen, audio: audio)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +32,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func Tap(imagen : UIImage, audio : String){
+        let urlSonido = URL(fileURLWithPath: Bundle.main.path(forResource: audio, ofType: "wav")!)
+        
+        do{
+            player = try AVAudioPlayer(contentsOf: urlSonido)
+            player.prepareToPlay()
+            player.play()
+        }catch{
+            
+        }
+    }
 }
 
